245bc2b0e9759f6be4777230faee4219dc061d5e
Riku Nummi <rikunummi9@gmail.com>
2026-01-07 10:55:51 +0200

    Add first dag

--- airflow/dags/elt_dag.py
+++ airflow/dags/elt_dag.py
@@ -1,14 +1,14 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from docker.types import Mount
-from airflow.operators.python_operators import PythonOperator
+from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
-from airflow.operators.docker import DockerOperator
+from airflow.providers.docker.operators.docker import DockerOperator
 import subprocess
 
 default_args = {
     'owner': 'airflow',
-    'depens_on_past': False,
+    'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
 }
@@ -24,11 +24,29 @@ def run_elt_script():
 dag = DAG(
     'elt_and_dbt',
     default_args=default_args,
-    description='A simple ELT and dbt DAG',
-    start_date=datetime(2026, 1, 6),
+    description='An ELT workflow with dbt',
+    start_date=datetime(2026, 1, 7),
     catchup=False,
 )
 
 t1 = PythonOperator(
-    
+    task_id="run_elt_script",
+    python_callable=run_elt_script,
+    dag=dag,
 )
+
+t2 = DockerOperator(
+    task_id="dbt_run",
+    image="ghcr.io/dbt-labs/dbt-postgres:1.4.7",
+    command=["run", "--profiles-dir", "/root", "--project-dir", "/dbt", "--profile", "custom_postgres", "--target", "dev"],
+    auto_remove="success",
+    docker_url="unix://var/run/docker.sock",
+    network_mode="bridge",
+    mounts=[
+        Mount(source="./custom_postgres", target="/dbt", type="bind"),
+        Mount(source="~/.dbt", target="/root", type="bind")
+    ],
+    dag=dag,
+)
+
+t1 >> t2
